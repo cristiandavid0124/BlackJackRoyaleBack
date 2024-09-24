832013127ed0acff361a887ff4f36ec085deb45e
Cristian <cristian.naranjo@mail.escuelaing.edu.co>
2024-09-24 18:49:59 +0200

    subiendo esquema aplicacion blackjackroyale back

---
+++ src/main/java/com/escuelaing/Game/BlackJackRoyaleApp.java
@@ -0,0 +1,13 @@
+package com.escuelaing.Game;
+
+/**
+ * Hello world!
+ *
+ */
+public class BlackJackRoyaleApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
